bf733c486bb61d1b238506563cb12f63a4104160
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-06-03 21:10:41 -0400

    about section done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
 import NavBar from './Layout/NavBar';
+import About from './Components/About/About';
 
 function App() {
   return (
     <>
         <NavBar/>
-    <div className="App">
+      <div className="App">
+        <About/>
       </div>
     </>
   );
