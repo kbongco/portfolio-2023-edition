6dd339ff8242aef6e5e06941ceb4f4b059a8b477
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-06-05 11:09:50 -0400

    completed mobile at mvp

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ export default function Contact() {
       {isEmployed ? 
         <div className='chbi-currently-employed-message'><p className='chbi-employed-text'>I am currently not looking for any new roles at the moment, but I am down to help out in some way or form and to chat! Send me a message below!</p></div> : <div className='chbi-unemployed-message'><p className='chbi-unemployed-text'>I am currently looking for new roles in front end development, UX Engineering, and full stack roles. If you like what you see or maybe just want to chat, send me a message below!</p> </div>}
       <div className='chbi-button-container chbi-button-container-contact'>
-      <a href='https://www.linkedin.com/in/kathleen-bongco-11843197/'><button className='chbi-button-contact'>Contact Me!</button></a>
+      <a href='https://forms.gle/ZsSQgBXtoL2XKHwN8'><button className='chbi-button-contact'>Contact Me!</button></a>
       </div>
     </div>
   )
