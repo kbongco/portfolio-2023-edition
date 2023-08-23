5fbe6d9b3f5b48ce1754d87dfab79731b43989da
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-08-23 12:00:39 -0400

    fixed up projects secrtion

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,10 @@
 import './Projects.scss'
+import samples from '../../assets/samples.png'
 export default function Projects() {
   return (
     <div className='chbi-projects-container' id='projects'>
       <h4 className="chbi-projects-header">Projects and Cool things</h4>
-      <p>Here are some projects I've worked on in the past and some of the cool stuff I am currently working and playing around with!</p>
+      <p>Here are some projects I've worked on in the past and some of the cool stuff I am currently working and playing around with! This is just a small set of projects I have worked on, if you would like to see some source code check out my <a className='chbi-github-link'href='http://github.com/kbongco'>Github</a>! </p>
       <div className='chbi-projects-container-details'>
         <img className='chbi-portfolio-site chbi-portfolio-site-mobile 'src="https://i.ibb.co/7QCdtM2/portfolio.png"/>
         <p>Portfolio Site<br />
@@ -17,11 +18,11 @@ Figma was used for prototyping </p>
 Built with: 
 Ruby/Rails for back end 
 React.js, Chart.js, SCSS, </p>
-        <img className='chbi-nookdepot-site'src='https://i.ibb.co/0fsxCr4/Screen-Shot-2023-06-04-at-2-53-20-PM.png'/>
-        <p>NookDepot<br />
-          The final project from my time at Coding Bootcamp. An Etsy clone themed to the game of animal crossing where users can set up shops, trade items, and look for seasonal goodies!
-          Built with: Ruby/Rails for back end
-        React.js and CSS</p>
+        <img className='chbi-nookdepot-site' src={samples} />
+        <p>Sample Submission<br />
+        <a  className='chbi-project-link' href='https://github.com/kbongco/sample-management-frontend'>Github Link</a> | Deployed Site<br/>
+          A passion project of mine that solves a problem that inspired me to go into tech from when I was working in the food industry! A CRUD application which replaces the usage of excel sheets which a lot of labs like to use to keep track of samples going in and out of testing. This will be a full stack application. React/Typescript is used for the front-end, Jest/React Testing Library is used to write tests, Figma is used for designing and prototyping, Storybook is used for components. Backend language is to be determined.<br/>
+        </p>
       </div>
     </div>
   )
