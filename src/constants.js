264c2c886fbf5d180600ec05d5184d04dae6e833
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-11-23 12:34:28 -0500

    redesigning the project section with a display card

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,37 @@ export const Languages = ['HTML', 'CSS', 'Javascript', 'Typescript', 'Python', '
 
 export const frameworksAndLibraries = ['React.js', 'Angular', 'Flask', 'Rails', 'RxJs', 'Redux', 'Express', 'Mongoose']
 
-export const tools = ['VSCode', 'Git/Github', 'Figma', 'Postman', 'Linux', 'NPM', 'Vite']
+export const tools = ['VSCode', 'Git/Github', 'Figma', 'Postman', 'Linux', 'NPM', 'Vite', 'Supabase']
 
 export const currentLearning = ['Next.js', 'GraphQL', 'NestJS', 'Improving my other skills']
+
+export const projects = [
+  {
+    projectName: 'Portfolio Site',
+    description: "Fun fact: this is the sixth rendition of my portfolio site. I decided to keep this very simple and not flashy. My portfolio site is a constant work in progress and I'm proud to say that this will be the best version yet! Built using: React.js, SCSS, Figma was used for prototyping",
+    projectImg: 'https://i.ibb.co/7QCdtM2/portfolio.png',
+    gitHub: 'https://github.com/kbongco/portfolio-2023-edition',
+    design: 'https://www.figma.com/file/Yp9xlAIujIOLDHaU1qypo4/Portfolio?type=design&node-id=0%3A1&mode=design&t=TQkccevRmVP9FImS-1'
+  },
+  {
+    projectName: 'WeebTrade',
+    description: 'An ecommerce site that allows users to trade and sell anime figures to each other. Currently a React front end with a Supabase back end, with hopes to convert to a Nest.js Back end in the near future',
+    projectImg: 'https://i.ibb.co/1JzxKsD/Screen-Shot-2023-11-23-at-12-07-44-PM.png',
+    github: 'https://github.com/kbongco/WeebTrade',
+    figmaLink: 'https://www.figma.com/file/pJFbA67zKc0XxbFYwv5p3J/WeebTrades?type=design&node-id=0%3A1&mode=design&t=oXgBrfNTpXaNqICR-1'
+  },
+  {
+    projectName: 'Weeb Jams',
+    desciption: 'A mobile first web application which utilizes the Jikan API and the Spotify API which allows users to search anime and their OP/ED and see if the song is available on Spotify. Built in React.js',
+    imgLink: 'https://i.ibb.co/dMCPFcV/Screen-Shot-2023-11-23-at-12-10-16-PM.png',
+    github: 'https://github.com/kbongco/Weeb-Jams',
+    design: 'https://www.figma.com/file/PIxXwl1qzONfq4wnta5kLU/WeebJams?type=design&node-id=4%3A27&mode=design&t=7nN7bcPYMgMx6E9r-1'
+  },
+  {
+    projectName: 'Sample Submission',
+    description: 'A web application idea that I came up with while still working in the food industry that can be used in laboratories to replace the usage of excel sheets when keeping track of samples going in and out of the lab. Built in React.JS, SCSS with a Backend TBD. ',
+    imgLink: 'https://i.ibb.co/W3ND1DM/Screen-Shot-2023-11-23-at-12-17-00-PM.png',
+    github: 'https://github.com/kbongco/sample-management-frontend',
+    design: ''
+  }
+]
