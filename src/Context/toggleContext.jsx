47a8c1e79a7bf221b357b0bf00404bb7b4f5e75e
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-04-09 14:41:24 -0400

    added in context

---
+++ src/Context/toggleContext.jsx
@@ -0,0 +1,20 @@
+// ThemeContext.js
+import React, { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("videoGame"); // Default theme
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => prevTheme === 'professional' ? 'videoGame' : 'professional');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
